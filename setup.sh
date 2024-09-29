aac9e70798cd122eb5423bee043ff0cec97ef737
Tobirou-Senju <a9998964@gmail.com>
2024-09-29 12:32:17 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,140 @@
+#!/bin/sh -e
+
+RC='\033[0m'
+RED='\033[31m'
+YELLOW='\033[33m'
+GREEN='\033[32m'
+
+# Check if the home directory and linuxtoolbox folder exist, create them if they don't
+LINUXTOOLBOXDIR="$HOME/linuxtoolbox"
+
+if [ ! -d "$LINUXTOOLBOXDIR" ]; then
+    echo "${YELLOW}Creating linuxtoolbox directory: $LINUXTOOLBOXDIR${RC}"
+    mkdir -p "$LINUXTOOLBOXDIR"
+    echo "${GREEN}linuxtoolbox directory created: $LINUXTOOLBOXDIR${RC}"
+fi
+
+if [ -d "$LINUXTOOLBOXDIR/mybash" ]; then rm -rf "$LINUXTOOLBOXDIR/mybash"; fi
+
+echo "${YELLOW}Cloning mybash repository into: $LINUXTOOLBOXDIR/mybash${RC}"
+git clone https://github.com/ChrisTitusTech/mybash "$LINUXTOOLBOXDIR/mybash"
+if [ $? -eq 0 ]; then
+    echo "${GREEN}Successfully cloned mybash repository${RC}"
+else
+    echo "${RED}Failed to clone mybash repository${RC}"
+    exit 1
+fi
+
+# add variables to top level so can easily be accessed by all functions
+PACKAGER=""
+SUDO_CMD=""
+SUGROUP=""
+GITPATH=""
+
+cd "$LINUXTOOLBOXDIR/mybash" || exit
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+checkEnv() {
+    ## Check for requirements.
+    REQUIREMENTS='curl groups sudo'
+    for req in $REQUIREMENTS; do
+        if ! command_exists "$req"; then
+            echo "${RED}To run me, you need: $REQUIREMENTS${RC}"
+            exit 1
+        fi
+    done
+
+    ## Check Package Handler
+    PACKAGEMANAGER='apt dnf yum pacman zypper emerge xbps-install nix-env'
+    for pgm in $PACKAGEMANAGER; do
+        if command_exists "$pgm"; then
+            PACKAGER="$pgm"
+            echo "Using $pgm"
+            break
+        fi
+    done
+
+    if [ -z "$PACKAGER" ]; then
+        echo "${RED}Can't find a supported package manager${RC}"
+        exit 1
+    fi
+
+    if command_exists sudo; then
+        SUDO_CMD="sudo"
+    elif command_exists doas && [ -f "/etc/doas.conf" ]; then
+        SUDO_CMD="doas"
+    else
+        SUDO_CMD="su -c"
+    fi
+
+    echo "Using $SUDO_CMD as privilege escalation software"
+
+    ## Check if the current directory is writable.
+    GITPATH=$(dirname "$(realpath "$0")")
+    if [ ! -w "$GITPATH" ]; then
+        echo "${RED}Can't write to $GITPATH${RC}"
+        exit 1
+    fi
+
+    ## Check SuperUser Group
+
+    SUPERUSERGROUP='wheel sudo root'
+    for sug in $SUPERUSERGROUP; do
+        if groups | grep -q "$sug"; then
+            SUGROUP="$sug"
+            echo "Super user group $SUGROUP"
+            break
+        fi
+    done
+
+    ## Check if member of the sudo group.
+    if ! groups | grep -q "$SUGROUP"; then
+        echo "${RED}You need to be a member of the sudo group to run me!${RC}"
+        exit 1
+    fi
+}
+
+installDepend() {
+    ## Check for dependencies.
+    DEPENDENCIES='bash bash-completion tar bat tree multitail wget unzip fontconfig'
+    echo "${YELLOW}Installing dependencies...${RC}"
+
+    if [ "$PACKAGER" = "dnf" ]; then
+        ${SUDO_CMD} ${PACKAGER} install -y ${DEPENDENCIES}
+    else
+        ${SUDO_CMD} ${PACKAGER} install -yq ${DEPENDENCIES}
+    fi
+
+    # Check to see if the MesloLGS Nerd Font is installed (Change this to whatever font you would like)
+    FONT_NAME="MesloLGS Nerd Font Mono"
+    if fc-list :family | grep -iq "$FONT_NAME"; then
+        echo "Font '$FONT_NAME' is installed."
+    else
+        echo "Installing font '$FONT_NAME'"
+        FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
+        FONT_DIR="$HOME/.local/share/fonts"
+        if wget -q --spider "$FONT_URL"; then
+            TEMP_DIR=$(mktemp -d)
+            wget -q --show-progress $FONT_URL -O "$TEMP_DIR"/"${FONT_NAME}".zip
+            unzip "$TEMP_DIR"/"${FONT_NAME}".zip -d "$TEMP_DIR"
+            mkdir -p "$FONT_DIR"/"$FONT_NAME"
+            mv "${TEMP_DIR}"/*.ttf "$FONT_DIR"/"$FONT_NAME"
+            fc-cache -fv
+            rm -rf "${TEMP_DIR}"
+            echo "'$FONT_NAME' installed successfully."
+        else
+            echo "Font '$FONT_NAME' not installed. Font URL is not accessible."
+        fi
+    fi
+}
+
+installStarshipAndFzf() {
+    if command_exists starship; then
+        echo "Starship already installed"
+        return
+    fi
+
+    if ! curl -sS https://starship.rs/install.sh
